83a50082edeb0270a4775588265024ae322e6e4d
Anshu <anshubaka2004@gmail.com>
2024-10-13 13:44:55 +0530

    home div solved and learnt m-auto p-auto

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,20 +3,26 @@ import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div
-      className=" w-full h-screen bg-violet-300 flex justify-start items-centre"
-    >
-      <Image
-        src="/images/hyunjin.jpg"
-        alt=""
-        height={303}
-        width={500}
-        className='ml-9 rounded-xl'
-      />
-      <p className="m-10 p-40">
-      The future of AI-powered "eyes" holds the promise of enhanced vision systems that can process and interpret visual data far beyond human capabilities. These systems could revolutionize industries such as healthcare, autonomous vehicles, surveillance, and robotics by providing real-time analysis and decision-making. AI eyes could aid in early disease detection, enable safer self-driving cars, and even allow machines to interact more intuitively with their environments. As technology advances, the combination of AI and visual sensors will likely reshape how we perceive and interact with the world.
-      </p>
-    </div>
+    <>
+      <div
+        className="w-full h-screen bg-indigo-950 flex justify-start items-centre"
+      >
+        <Image
+          src="/images/hyunjin.jpg"
+          alt=""
+          height={400}
+          width={500}
+          className='m-auto p-14' />
+        <p className="m-auto p-40 text-lime-200">
+          The future of AI-powered "eyes" holds the promise of enhanced vision systems that can process and interpret visual data far beyond human capabilities. These systems could revolutionize industries such as healthcare, autonomous vehicles, surveillance, and robotics by providing real-time analysis and decision-making. AI eyes could aid in early disease detection, enable safer self-driving cars, and even allow machines to interact more intuitively with their environments. As technology advances, the combination of AI and visual sensors will likely reshape how we perceive and interact with the world.
+        </p>
+      </div>
+      <br className='bg-black'></br>
+      <div className='w-full h-screen bg-indigo-950'>
+        yo
+      </div>
+
+    </>
 
   )
 }
