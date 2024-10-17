c72a180c5561d296323ffacaa1438ffe40661c5b
Anshu <anshubaka2004@gmail.com>
2024-10-17 11:44:27 +0530

    Update contact
    Contact update
    
    Co-Authored-By: Anshu Aditya <80463220+anshu2004@users.noreply.github.com>

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     // Will make with the help of template 
+    // In centre of page and some 3d effects
     <div>Contact</div>
   )
 }
